387e61f6545b36ab5a7843c6200c79541fa2e76f
BenCree <benstuartcree@gmail.com>
2023-10-31 16:51:52 +0000

    removing old folders

--- nsp13_al/run.sh
+++
@@ -1,6 +0,0 @@
-export FG_ANIMODEL_PATH=/home/dresio/newcastle/sarscov2_reconstruction_random_search/random_dask/animodel.pt
-export FG_GNINA_PATH=/home/dresio/gnina
-
-export FG_SCALE=5
-
-python rsearcher.py
