c1f7e5b0b6ca441719f0239767513609b0e5f904
bieniekmat <bieniekmat@gmail.com>
2024-01-26 17:34:09 +0000

    Full code done, time to clean up

--- sars-cov-2-main-protease-al-study-plip/plip_tests/run_plip_ben.py
+++ sars-cov-2-main-protease-al-study-plip/plip_tests/run_plip_ben.py
@@ -77,7 +77,7 @@ def residue_interactions(pdb_id):
 
     all_interactions = all_interactions.reset_index()
     all_interactions = all_interactions.rename(columns={"level_0": "type", "level_1": f"id"})
-    print(all_interactions)
+    # print(all_interactions)
     # oh = pd.get_dummies(a.index)  #onehot encode at some point potentially??
 
     #    res_interactions = all_interactions.groupby('RESNR')[['type','RESTYPE', 'DIST', 'ANGLE',]].agg(lambda x: list(x)).reset_index()
@@ -165,33 +165,33 @@ def plip_score(ref_set, var_set):
     return tanimoto_similarity
 
 # xstal interactions from 24 mpro fragments
-xstal_interaction_list = [   'hbond_ASN_142_O2',
-                             'hbond_ASN_142_O3',
-                             'hbond_CYS_145_O3', #
-                             'hbond_CYS_44_O2',
-                             'hbond_GLN_189_O2',
-                             'hbond_GLN_189_O3',
-                             'hbond_GLU_166_N3',
-                             'hbond_GLU_166_O.co2',
-                             'hbond_GLU_166_O2',
-                             'hbond_GLU_166_O3',
-                             'hbond_GLY_143_O3',
-                             'hbond_HIS_163_N2',
-                             'hbond_HIS_41_O2',
-                             'hbond_SER_144_O3',
-                             'hbond_THR_25_N3',
-                             'hbond_THR_25_O3',
-                             'hydrophobic_ASP_187_nan',
-                             'hydrophobic_GLN_189_nan',
-                             'hydrophobic_GLU_166_nan',
-                             'hydrophobic_HIS_41_nan',
-                             'hydrophobic_MET_165_nan',
-                             'hydrophobic_PHE_140_nan',
-                             'hydrophobic_PRO_168_nan',
-                             'hydrophobic_THR_25_nan',
-                             'pistacking_HIS_41_nan',
-                             'saltbridge_GLU_166_nan']
-xstal_set = set(xstal_interaction_list)
+mpro_crystal_structures_interactions = {'hacceptor_THR_25_8',
+                                         'hdonor_THR_25_8',
+                                         'hydrophobic_THR_25',
+                                         'hacceptor_HIS_41_8',
+                                         'hydrophobic_HIS_41',
+                                         'pistacking_HIS_41',
+                                         'hacceptor_CYS_44_8',
+                                         'hydrophobic_PHE_140',
+                                         'hacceptor_ASN_142_8',
+                                         'hdonor_ASN_142_7',
+                                         'hdonor_GLY_143_7',
+                                        'hacceptor_SER_144_8',
+                                        'hdonor_SER_144_7',
+                                        'hdonor_SER_144_8',
+                                        'hdonor_CYS_145_7',
+                                        'hacceptor_HIS_163_7',
+                                        'hydrophobic_MET_165',
+                                        'hacceptor_GLU_166_8',
+                                        'hdonor_GLU_166_7',
+                                        'hdonor_GLU_166_8',
+                                        'hydrophobic_GLU_166',
+                                        'saltbridge_GLU_166',
+                                        'hydrophobic_PRO_168',
+                                        'hydrophobic_ASP_187',
+                                        'hacceptor_GLN_189_8',
+                                        'hdonor_GLN_189_7',
+                                        'hydrophobic_GLN_189'}
 
 def gen_intrns_dict(interactions):
     """
@@ -207,7 +207,6 @@ def gen_intrns_dict(interactions):
     interactions['interaction'] = interactions.apply(
         lambda row: f"{row['type']}_{row['RESTYPE']}_{str(row['RESNR'])}_{row['ACCEPTORTYPE']}", axis=1)
     interaction_tuples = []
-    print(interaction_tuples)
     for interaction in interactions['interaction']:
         match = re.search('_(\w+)_(\d+)', interaction)
         if match:
@@ -268,7 +267,6 @@ def gen_intrns_dict(interactions):
 
 
 # ok code it
-print('hi')
 # from plip.structure.preparation import PDBComplex
 # my_mol = PDBComplex()
 # my_mol.load_pdb(complex_path) # Load the PDB file into PLIP class
@@ -280,27 +278,70 @@ print('hi')
 # # take all the interactions
 # all_interactions = list(my_mol.interaction_sets.values())[0]
 
-def gen_plip_fingerprint(complex_path):
-    my_mol = PDBComplex()
-    my_mol.load_pdb(complex_path) # Load the PDB file into PLIP class
-    my_mol.analyze()
 
-    # assert there is only one ligand for now
-    if len(my_mol.interaction_sets) != 1:
+def gen_plip_mpro_score(complex_path):
+    """
+    Get all the protein interactions from the interactions between the protein and the ligand
+
+    Args:
+        complex_path:
+
+    Returns: A list of strings
+    """
+    complex = PDBComplex()
+    complex.load_pdb(complex_path) # Load the PDB file into PLIP class
+    complex.analyze()
+
+    # assume there is only one ligand for now
+    if len(complex.interaction_sets) != 1:
         raise ValueError("PLIP detected more (or less) than one ligand?!")
-    binding_site = BindingSiteReport(list(my_mol.interaction_sets.values())[0])
+
+    # pair key and values
+    interactions = list(complex.interaction_sets.values())[0]
 
     # take all the interactions
-    all_interactions = list(my_mol.interaction_sets.values())[0]
-    print('hi')
-    pass
+    hydrophobic_contacts = ["hydrophobic_" + c.restype + "_" + str(c.resnr) for c in interactions.hydrophobic_contacts]
+    # extract protein donors
+    hdonors = ["hdonor_" + d.restype + "_" + str(d.resnr) + "_" + str(d.d.atomicnum) for d in interactions.hbonds_pdon]
+    # extract protein acceptors
+    hacceptors = ["hacceptor_" + a.restype + "_" + str(a.resnr) + "_" + str(a.a.atomicnum) for a in interactions.hbonds_ldon]
+    pistacking = ["pistacking_" + r.restype + "_" + str(r.resnr) for r in interactions.pistacking]
+    saltbridge = ["saltbridge_" + r.restype + "_" + str(r.resnr) for r in  interactions.saltbridge_pneg]
+    waterbridge = ["waterbridge_" + r.restype + "_" + str(r.resnr) for r in interactions.water_bridges]
+    pication = ["pication_" + r.restype + "_" + str(r.resnr) for r in interactions.pication_paro]
+    halogen_bond = ["halogenbond_" + r.restype + "_" + str(r.resnr) for r in interactions.halogen_bonds]
+    metal_complex = ["metalcomplex_" + r.restype + "_" + str(r.resnr) for r in interactions.metal_complexes]
+
+    protein_interaction_fingerprints = (hydrophobic_contacts +
+                                        hdonors +
+                                        hacceptors +
+                                        pistacking +
+                                        saltbridge +
+                                        waterbridge +
+                                        pication +
+                                        halogen_bond +
+                                        metal_complex)
+
+    intersection = len(mpro_crystal_structures_interactions.intersection(protein_interaction_fingerprints))
+    count = len(mpro_crystal_structures_interactions) + len(protein_interaction_fingerprints)
+    tanimoto_distance = intersection / (count - intersection)
+
+    return protein_interaction_fingerprints, tanimoto_distance
+
 
 contacts = set()
 for complex_path in Path("extracting_reference").glob("*pdb"):
-    plip_fp = gen_plip_fingerprint(str(complex_path))
-    print('hi')
-    # plip_score(xstal_set, data_interactions) * 50  # HYPERPARAM TO CHANGE
+    plip_dict = gen_intrns_dict(residue_interactions(str(complex_path)))
 
+    plip_fp, tanimoto_distance = gen_plip_mpro_score(str(complex_path))
+    print(plip_fp)
+    print(tanimoto_distance)
+    contacts = contacts.union(plip_dict)
+
+
+# contacts = list(contacts)
+# contacts.sort(key=lambda r: [int(r.split('_')[2]), r.split('_')[0], r.split('_')[1]])
+# print(contacts)
 
 # contacts = set()
 # for complex_path in Path("extracting_reference").glob("*pdb"):
